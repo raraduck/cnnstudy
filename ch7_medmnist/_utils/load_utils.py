8dc94370a0f02ee8ef94474f4d7d236087cd9a10
dwnusa-win10 <dwnusa@gmail.com>
2023-08-02 16:15:51 +0900

    update

--- ch7_medmnist/_utils/load_utils.py
+++ ch7_medmnist/_utils/load_utils.py
@@ -1,8 +1,9 @@
-# ch4_dataloader/utils/load_utils.py
-import numpy as np
-import matplotlib.pyplot as plt
+# ch7_medmnist/_utils/load_utils.py
 
-def compare_validations(all_acc_dict):
+import matplotlib.pyplot as plt
+import numpy as np
+
+def compare_validations(current_time, all_acc_dict):
     trn_acc = [v['train'] for k, v in all_acc_dict.items()]
     val_acc = [v['val'] for k, v in all_acc_dict.items()]
     
@@ -14,17 +15,25 @@ def compare_validations(all_acc_dict):
     plt.ylabel('accuracy')
     plt.legend(loc='lower right')
     plt.ylim(0.7, 1)
-    plt.savefig('./fig/accuracy_comparison.png', bbox_inches='tight')
+    plt.savefig(f'./results/{current_time}_accuracy_comparison.png', bbox_inches='tight')
     plt.show()
 
 
-def display_from_batch(sample_batch, nrows=1, ncols=1):
+def display_from_batch(sample_batch, nrows=1, ncols=1, CLASSES=None, ):
     fig, axes = plt.subplots(nrows, ncols, sharex=True, sharey=True)
-    axes_1d = axes.ravel()
+    if nrows==1 and ncols==1:
+        axes_1d = [axes]
+    else:
+        axes_1d = axes.ravel()
     for r in range(nrows):
         for c in range(ncols):
             i = ncols * r + c
             (imgs, cls) = sample_batch
-            axes_1d[i].imshow(imgs[i,:].permute(1,2,0))
-            axes_1d[i].set_title(cls[i].item())
+            axes_1d[i].imshow(imgs[i,:].permute(1,2,0), cmap='gray')
+            if CLASSES==None:
+                axes_1d[i].set_title(cls[i].tolist())
+            else:
+                axes_1d[i].set_title(CLASSES[cls[i].item()])
     plt.tight_layout()
+
+
