97a41d3b4feaa15be1d19f8b7e3d713d0546e388
dwnusa-win10 <dwnusa@gmail.com>
2023-08-05 10:06:17 +0900

    AlexNet

--- ch7_medmnist/loader/load_data.py
+++ ch7_medmnist/loader/load_data.py
@@ -105,3 +105,27 @@ def load_chestmnist(batch_size=256):
     test_loader = DataLoader(test, batch_size=batch_size, shuffle=False, drop_last=True)
 
     return train_loader, test_loader, classnames
+
+
+
+def load_cifar10(batch_size=256):
+    train = CIFAR10(root="./_data", train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+    test = CIFAR10(root="./_data", train=False, download=True, transform=transforms.Compose([transforms.ToTensor()]))
+
+    classnames={
+        '0': 'airplane', 
+        '1': 'automobile', 
+        '2': 'bird', 
+        '3': 'cat', 
+        '4': 'deer', 
+        '5': 'dog', 
+        '6': 'frog', 
+        '7': 'horse', 
+        '8': 'ship', 
+        '9': 'truck', 
+    }
+    
+    train_loader = DataLoader(train, batch_size=batch_size, shuffle=True, drop_last=True)
+    test_loader = DataLoader(test, batch_size=batch_size, shuffle=False, drop_last=True)
+
+    return train_loader, test_loader, classnames
