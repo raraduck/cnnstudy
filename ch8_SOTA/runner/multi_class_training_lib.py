0d94026d5a57bb3eb8b43e35501d8a58932f702f
dwnusa-win10 <dwnusa@gmail.com>
2023-08-16 17:32:32 +0900

    multi_class_train_lib

--- ch8_SOTA/runner/multi_class_training_lib.py
+++ ch8_SOTA/runner/multi_class_training_lib.py
@@ -5,19 +5,25 @@ import matplotlib.pyplot as plt
 
 def training_3min(model, train_loader, loss_fn, optimizer, epoch, device):
     model.train()
+    running_loss = 0.0
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
-        optimizer.zero_grad()
         output = model(data)
         # loss = F.cross_entropy(output, target)
         loss = loss_fn(output, target)
+        
+        optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
+        running_loss += loss.item()
+        
+    train_loss = running_loss / len(train_loader)
+    return train_loss
 
 def evaluate_3min(model, test_loader, loss_fn, device):
     model.eval()
-    test_loss = 0
+    running_loss = 0
     correct = 0
     with torch.no_grad():
         for data, target in test_loader:
@@ -25,51 +31,79 @@ def evaluate_3min(model, test_loader, loss_fn, device):
             output = model(data)
             
             # 배치 오차를 합산
-            test_loss += F.cross_entropy(output, target, reduction='sum').item()
-            # test_loss += loss_fn(output, target, reduction='sum').item()
+            # running_loss += F.cross_entropy(output, target, reduction='sum').item()
+            running_loss += loss_fn(output, target).item()
             
             # 가장 높은 값을 가진 인덱스가 바로 예측값
             pred = output.max(1, keepdim=True)[1]
             correct += pred.eq(target.view_as(pred)).sum().item()
     
-    test_loss /= len(test_loader.dataset)
+    test_loss = running_loss / len(test_loader)
     test_accuracy = 100. * correct / len(test_loader.dataset)
     return test_loss, test_accuracy
 
 
 def mclass_training_loop_3min(n_epochs, model, loss_fn, optimizer, scheduler, train_loader, test_loader, device='cpu'):
-    EPOCHS=n_epochs
-    for epoch in range(1, EPOCHS + 1):
+    train_losses = []
+    for epoch in range(1, n_epochs + 1):
         scheduler.step()
-        training_3min(model, train_loader, loss_fn, optimizer, epoch, device)
+        train_loss = training_3min(model, train_loader, loss_fn, optimizer, epoch, device)
+        train_losses.append(train_loss)
         test_loss, test_accuracy = evaluate_3min(model, test_loader, loss_fn, device)
-
         print(f'[{epoch}] Test Loss: {test_loss}, Accuracy: {test_accuracy}%')
         
         
-
-def mclass_training_loop_basic(n_epochs, model, loss_fn, optimizer, scheduler, train_loader, test_loader, device='cpu'):
-    loss_ = []
-    n = len(train_loader)
-    for epoch in range(n_epochs):
         
-        model.train()
-        running_loss = 0.0
-        for data in train_loader:
-            inputs, labels = data[0].to(device), data[1].to(device)
-            outputs = model(inputs)
-            loss = loss_fn(outputs, labels)
-            
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
-            
-            running_loss += loss.item()
+        
+        
+        
+        
+        
+def training_basic(model, train_loader, loss_fn, optimizer, epoch, device):
+    model.train()
+    running_loss = 0.0
+    for batch_idx, (data, target) in enumerate(train_loader):
+        data, target = data.to(device), target.to(device)
+        output = model(data)
+        loss = loss_fn(output, target)
+        
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
 
-        loss_.append(running_loss / n)
-        print('[%d] loss: %.3f' %(epoch + 1, running_loss /len(train_loader)))
+        running_loss += loss.item()
+        
+    train_loss = running_loss / len(train_loader)
+    return train_loss
+
     
-    plt.plot(loss_)
-    plt.title("Training Loss")
-    plt.xlabel("epoch")
-    plt.show()
+def evaluate_basic(model, test_loader, loss_fn, device):
+    model.eval()
+    running_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in test_loader:
+            images, labels = data[0].to(device), data[1].to(device)
+            outputs = model(images)
+            
+            running_loss += loss_fn(outputs, labels).item()
+            
+            _, predicted = torch.max(outputs, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+            
+    test_loss = running_loss / len(test_loader)
+    test_accuracy = 100. * correct / total
+    return test_loss, test_accuracy
+    
+    
+def mclass_training_loop_basic(n_epochs, model, loss_fn, optimizer, scheduler, train_loader, test_loader, device='cpu'):
+    train_losses = []
+    for epoch in range(1, n_epochs + 1):
+        scheduler.step()
+        train_loss = training_basic(model, train_loader, loss_fn, optimizer, epoch, device)
+        train_losses.append(train_loss)
+        test_loss, test_accuracy = evaluate_basic(model, test_loader, loss_fn, device)
+        print(f'[{epoch}] Test Loss: {test_loss}, Accuracy: {test_accuracy}%')
+    
